f3697dca0265f55d774e665eac99ce1f18d92be2
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-01-20 20:59:45 -0300

    feat: add modal error

---
+++ src/components/modalError/modalError.js
@@ -0,0 +1,22 @@
+function showNotificationError(message) {
+  if (document.getElementById("notificationErrorContainer")) {
+    document.getElementById("notificationErrorContainer").remove();
+  }
+
+  const containerError = document.createElement(`div`);
+  containerError.innerText = message;
+  containerError.id = "notificationErrorContainer";
+  document.querySelector(`body`).appendChild(containerError);
+
+  const closeButton = document.createElement(`div`);
+  closeButton.id = "notificationErrorButton";
+  closeButton.addEventListener("click", closeNotification);
+  containerError.appendChild(closeButton);
+
+  function closeNotification() {
+    containerError.remove();
+    clearTimeout(closeNotificationTimeout);
+  }
+
+  const closeNotificationTimeout = setTimeout(closeNotification, 5 * 1000);
+}
