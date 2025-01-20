3fe728e5e45b9571691e2d968d0ff6896940eac8
Anderson <andersonpaulojr1@gmail.com>
2025-01-19 21:24:45 -0300

    feat: add randomizer class
    This class does:
    - Set a list
    - Pick a random item from the list
    - Add winners to attributes
    - Reset the winners attributes
    - Generate a list with numbers within a range

---
+++ src/utils/randomizer.js
@@ -0,0 +1,58 @@
+export class Randomizer {
+    constructor(){
+        this._list = []
+        this._allWinners = []
+        this._currentWinners = []
+    }
+
+    set list(value){
+        this._list = value
+        this._allWinners = []
+        this._currentWinners = []
+    }
+
+    pickValues(number = 1){
+        this._currentWinners = []
+
+        if(this._list.length == 0){
+            throw new Error("There is no values in your list")
+        }
+        else if(number > this._list.length - this._allWinners.length){
+            throw new Error("You can't pick more values than the size of your list")
+        }
+        else {
+            let i = 0
+            while(i < number){
+                const pickedValue = this._list[(Math.random() * (this._list.length - 1)).toFixed(0)]
+                if(!this._allWinners.includes(pickedValue) && !this._currentWinners.includes(pickedValue)){
+                    this._currentWinners.push(pickedValue)
+                    i++;
+                }
+            }
+        }
+
+        return this._currentWinners
+    }
+
+    addCurrentWinners(){
+        for(const winner of this._currentWinners){
+            this._allWinners.push(winner)
+        }
+        this._currentWinners = []
+    }
+
+    resetWinners(){
+        this._allWinners = []
+        this._currentWinners = []
+    }
+
+    generateNumberList(min, max){
+        this._list = []
+        this._allWinners = []
+        this._currentWinners = []
+        for(let i = min; i <= max; i++){
+            this._list.push(i);
+        }
+    }
+}
+
