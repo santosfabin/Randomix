85321a5ac5f9a355354a5ed7925d945c8212f22a
Anderson <andersonpaulojr1@gmail.com>
2025-01-27 14:55:23 -0300

    feat:added SlotMachine to app & styles to elements

---
+++ src/pages/slot/index.js
@@ -0,0 +1,21 @@
+import {Randomizer} from '../../utils/randomizer.js'
+import SlotMachine from './SlotMachine.js'
+import {checkScreenSize, removeSlot} from './util.js'
+
+const sortear = new Randomizer()
+const appDiv = document.getElementById("app")
+let lista = ["Anderson", "Gabriel"]
+sortear.list = lista;
+
+appDiv.appendChild(SlotMachine(sortear))
+
+window.addEventListener("resize", e => {
+    const showSlotItens = document.querySelectorAll(".showSlotItem")
+    const showSlotItensLenght = document.querySelectorAll(".showSlotItem").length
+    if(showSlotItensLenght > checkScreenSize(window.innerWidth)){
+        removeSlot()
+    }
+})
+
+
+
