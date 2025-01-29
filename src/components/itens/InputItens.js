f4077b5c8427720778edae72b11dbfca110844b6
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-01-28 21:25:12 -0300

    feat: add commands to "Input Items" module and change header module name

--- src/components/itens/InputItens.js
+++ src/components/itens/InputItens.js
@@ -1,31 +1,45 @@
-import {Button} from '../button/button.js'
+import { Button } from "../button/button.js";
 
-function InputItens(randomizer, newContainer){
-    const container = document.createElement("div")
-    const textArea = document.createElement("textarea")
+function InputItens(randomizer, newContainer) {
+  const container = document.createElement("div");
+  const textArea = document.createElement("textarea");
 
-    container.classList.add("containerItens")
-    textArea.classList.add("textAreaItens")
-    textArea.placeholder = "Separe os itens por vírgula"
+  container.classList.add("containerItens");
+  textArea.classList.add("textAreaItens");
+  textArea.placeholder = "Separe os itens por vírgula";
 
-    container.appendChild(textArea)
-    container.appendChild(Button("PLAY", "/src/assets/button/playButton.svg", "linear-gradient(to top, #0ba360 0%, #3cba92 100%)", () => {getList(textArea.value, randomizer, container, newContainer)}))
+  container.appendChild(textArea);
+  container.appendChild(
+    Button(
+      "PLAY",
+      "/src/assets/button/playButton.svg",
+      "linear-gradient(to top, #0ba360 0%, #3cba92 100%)",
+      () => {
+        getList(textArea.value, randomizer, container, newContainer);
+      }
+    )
+  );
 
-    return container
+  return container;
 }
 
-function getList(text, randomizer, containerTextArea, newContainer){
-    let list = text.replace(", ", ",").split(",")
-    list = list.map(element => {
-        let item = element.trim()
-        item = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
-        return item}).filter(element => element != "")
-    list = Array.from(new Set(list))
-    if(list.length <= 1){
-        alert("Coloque no mínimo dois itens na lista")
-        return
-    }
-    randomizer.list = list
-    containerTextArea.remove()
-    document.getElementById("app").appendChild(newContainer)
+function getList(text, randomizer, containerTextArea, newContainer) {
+  let list = text.replace(", ", ",").split(",");
+  list = list
+    .map((element) => {
+      let item = element.trim();
+      item = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase();
+      return item;
+    })
+    .filter((element) => element != "");
+  list = Array.from(new Set(list));
+  if (list.length <= 1) {
+    alert("Coloque no mínimo dois itens na lista");
+    return;
+  }
+  randomizer.list = list;
+  containerTextArea.remove();
+  document.getElementById("app").appendChild(newContainer);
 }
+
+export { InputItens };
