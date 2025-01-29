9a40aeb82c0e7c08366003a4c6c9dd58fb8610bf
Anderson <andersonpaulojr1@gmail.com>
2025-01-29 15:34:48 -0300

    feat: add components to SlotPage

--- src/pages/slot/index.js
+++ src/pages/slot/index.js
@@ -1,13 +1,37 @@
 import {Randomizer} from '../../utils/randomizer.js'
 import SlotMachine from './SlotMachine.js'
 import {checkScreenSize, removeSlot} from './util.js'
+import {InputItens} from '../../components/itens/InputItens.js'
+import {WinnersList} from '../../components/winnersList/winnersList.js'
+import {Button} from '../../components/button/button.js'
 
-const sortear = new Randomizer()
-const appDiv = document.getElementById("app")
-let lista = ["Anderson", "Gabriel"]
-sortear.list = lista;
+function SlotPage(){
+    const sortear = new Randomizer()
+    const containerSlotPage = document.createElement("div")
+    const inputItens = InputItens(sortear, containerSlotPage)
+    const slotMachine = SlotMachine(sortear)
+    const winnersList = WinnersList()
+    const buttonRETURN = Button(
+    "RETURN",
+    "/src/assets/button/returnButton.svg",
+    "linear-gradient(-225deg, #A445B2 0%, #D41872 52%, #FF0066 100%)",
+    () => {resetPage(containerSlotPage)}
+    );
+    containerSlotPage.id = "SlotPage"
+    inputItens.id = "inputSlotPage"
+    buttonRETURN.classList.add("buttonRETURN")
+    containerSlotPage.appendChild(slotMachine)
+    containerSlotPage.appendChild(buttonRETURN)
+    containerSlotPage.appendChild(winnersList)
 
-appDiv.appendChild(SlotMachine(sortear))
+    return inputItens
+}
+
+function resetPage(containerPage){
+    const randoMixDiv = document.getElementById("randomix")
+    containerPage.remove()
+    randoMixDiv.appendChild(SlotPage())
+}
 
 window.addEventListener("resize", e => {
     const showSlotItens = document.querySelectorAll(".showSlotItem")
