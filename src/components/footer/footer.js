fd95acf3f228b6403e95faa47a41662b9c2f18da
Gabriell Queiroz <gabriellqs10@gmail.com>
2025-01-30 20:48:35 -0300

    refector: refactoring the code and design of the footer element

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,44 +1,66 @@
 function Footer() {
-	const footer = `
-    <div class="limiter">
-      <section>
-        <h3>Github</h3>
-        <ul>
-          <a href="https://github.com/santosfabin/Randomix" target="_blank">
-            <li>Repositório</li>
-          </a>
-        </ul>
-      </section>
+  const divFooter = document.createElement("footer");
 
-      <section>
-        <h3>Desenvolvedores</h3>
-        <ul>
-          <a href="https://github.com/andersonjr1" target="_blank">
-            <li>Anderson</li>
-          </a>
-          <a href="https://github.com/santosfabin" target="_blank">
-            <li>Fabiano</li>
-          </a>
-          <a href="https://github.com/gbrQueiroz" target="_blank">
-            <li>Gabriell</li>
-          </a>
-        </ul>
-      </section>
+  const sectionFooter = document.createElement("div");
+  sectionFooter.id = "sectionFooter"
+  divFooter.appendChild(sectionFooter);
 
-      <section>
-        <h3>Alpha EdTech</h3>
-        <ul>
-          <a href="https://www.alphaedtech.org.br/" target="_blank">
-            <li>Conheça a Alpha</li>
-          </a>
-        </ul>
-      </section>
-    </div>
-`;
-	const divFooter = document.createElement("footer");
-	divFooter.innerHTML = footer;
+  const innerFooterSection = document.createElement("div");
+  innerFooterSection.id = "innerFooterSection"
+  sectionFooter.appendChild(innerFooterSection);
 
-	return divFooter;
+  const innerSection = document.createElement("div")
+  innerFooterSection.appendChild(innerSection)
+
+  const gitHubImage = document.createElement("img");
+  gitHubImage.src = "/src/assets/footer/gitHubImage.png";
+  gitHubImage.alt = "Github";
+  innerSection.appendChild(gitHubImage);
+
+  const linksSection = document.createElement("div");
+  innerSection.appendChild(linksSection);
+
+  const projectLink = document.createElement("a");
+  projectLink.href = "https://github.com/santosfabin/Randomix";
+  projectLink.target = "_blank";
+  projectLink.textContent = "Project";
+  linksSection.appendChild(projectLink);
+
+  const andersonLink = document.createElement("a");
+  andersonLink.href = "https://github.com/andersonjr1";
+  andersonLink.target = "_blank";
+  andersonLink.textContent = "Anderson";
+  linksSection.appendChild(andersonLink);
+
+  const fabianoLink = document.createElement("a");
+  fabianoLink.href = "https://github.com/santosfabin";
+  fabianoLink.target = "_blank";
+  fabianoLink.textContent = "Fabiano";
+  linksSection.appendChild(fabianoLink);
+
+  const gabriellLink = document.createElement("a");
+  gabriellLink.href = "https://github.com/gbrQueiroz";
+  gabriellLink.target = "_blank";
+  gabriellLink.textContent = "Gabriell";
+  linksSection.appendChild(gabriellLink);
+
+  const alphaImageLink = document.createElement("a");
+  alphaImageLink.href = "https://www.alphaedtech.org.br/";
+  alphaImageLink.target = "_blank";
+  innerFooterSection.appendChild(alphaImageLink);
+
+  const alphaImage = document.createElement("img");
+  alphaImage.src = "/src/assets/footer/alphaImage.png";
+  alphaImage.alt = "AplhaEdtech Site";
+  alphaImageLink.appendChild(alphaImage);
+
+  const copyrightText = document.createElement("p");
+  copyrightText.textContent = "© 2024 - All rights reserved.";
+  sectionFooter.appendChild(copyrightText);
+
+  return divFooter;
 }
 
-export {Footer};
+document.querySelector("body").appendChild(Footer());
+
+export { Footer };
